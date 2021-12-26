bed214f1231c20882e01f4c3b1a664e3de5d2624
embersongs <67457948+embersongs@users.noreply.github.com>
2021-12-26 09:36:41 +0300

    Add files via upload

---
+++ Скрипты/ObjColliderCreate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjColliderCreate : MonoBehaviour
+{
+    public GameObject obj;
+   
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Столкновение");
+        Instantiate(
+            obj, //что создавать
+            other.gameObject.transform.position, //где создавать
+            other.gameObject.transform.rotation // с каким поворотом
+            );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Скрипты/spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+
+    public GameObject obj;
+
+    void Start()
+    {
+        InvokeRepeating("Spawn", 0, 0.02f);
+    }
+
+    
+    void Spawn()
+    {
+        Instantiate(obj, transform.position, transform.rotation);
+    }
+
+    private void Update()
+    {
+
+    }
+}

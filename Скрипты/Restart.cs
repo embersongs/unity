30c45b4a71f1bf78f58d0616d1e1c4ea80835b18
embersongs <67457948+embersongs@users.noreply.github.com>
2025-03-21 14:46:22 +0700

    Add files via upload

---
+++ Скрипты/Restart.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        // Проверяем, что объект, вошедший в триггер, имеет тег "Finish"
+        if (other.CompareTag("Player"))
+        {
+            // Перезагружаем текущую сцену
+            RestartLevel();
+        }
+    }
+
+    // Метод для перезагрузки уровня
+    private void RestartLevel()
+    {
+        // Получаем индекс текущей сцены
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        // Перезагружаем сцену по её индексу
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+}
